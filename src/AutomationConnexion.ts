e802036ba03c6b608a7f7067d8d58c2be7f9a681
evandikt <evandikt@gmail.com>
2021-09-16 16:18:24 +0200

    Update to TCP

---
+++ src/AutomationConnexion.ts
@@ -0,0 +1,96 @@
+import { EvntComNode } from "evntcom-js/dist/node";
+import robot from "robotjs";
+
+export class AutomationConnexion {
+  private name: string;
+  private evntCom: EvntComNode;
+
+  constructor(evntBoardHost: string, evntBoardPort: number, name: string) {
+    this.name = name;
+    this.evntCom = new EvntComNode({
+      name,
+      port: evntBoardPort,
+      host: evntBoardHost,
+    });
+
+    this.evntCom.onOpen = async () => {
+      await this.evntCom.notify("newEvent", [
+        "automation-load",
+        null,
+        { emitter: this.name },
+      ]);
+    };
+
+    this.evntCom.expose("setKeyboardDelay", async (ms: number) =>
+      robot.setKeyboardDelay(ms)
+    );
+
+    this.evntCom.expose(
+      "keyTap",
+      async (key: string, modifier?: string | string[]) =>
+        robot.keyTap(key, modifier)
+    );
+
+    this.evntCom.expose(
+      "keyToggle",
+      async (key: string, down: string, modifier?: string[]) =>
+        robot.keyToggle(key, down, modifier)
+    );
+
+    this.evntCom.expose("typeString", async (message: string) =>
+      robot.typeString(message)
+    );
+
+    this.evntCom.expose(
+      "typeStringDelayed",
+      async (message: string, cpm: number) =>
+        robot.typeStringDelayed(message, cpm)
+    );
+
+    // mouse
+
+    this.evntCom.expose("setMouseDelay", async (ms: number) =>
+      robot.setMouseDelay(ms)
+    );
+
+    this.evntCom.expose("moveMouse", async (x: number, y: number) =>
+      robot.moveMouse(x, y)
+    );
+
+    this.evntCom.expose("moveMouseSmooth", async (x: number, y: number) =>
+      robot.moveMouseSmooth(x, y)
+    );
+
+    this.evntCom.expose("mouseClick", async (side: string, double: boolean) =>
+      robot.mouseClick(side, double)
+    );
+
+    this.evntCom.expose("mouseToggle", async (down: string, button: string) =>
+      robot.mouseToggle(down, button)
+    );
+
+    this.evntCom.expose("dragMouse", async (x: number, y: number) =>
+      robot.dragMouse(x, y)
+    );
+
+    this.evntCom.expose("getMousePos", async () => robot.getMousePos());
+
+    this.evntCom.expose("scrollMouse", async (x: number, y: number) =>
+      robot.scrollMouse(x, y)
+    );
+
+    // screen
+
+    this.evntCom.expose("getPixelColor", async (x: number, y: number) =>
+      robot.getPixelColor(x, y)
+    );
+
+    this.evntCom.expose("getScreenSize", async () => robot.getScreenSize());
+
+    this.evntCom.expose(
+      "screenCapture",
+      async (x: number, y: number, width: number, height: number) =>
+        robot.screen.capture(x, y, width, height)
+    );
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,13 @@
-require("dotenv").config();
-import { EvntComNode } from "evntcom-js/dist/node";
-import robot from "robotjs";
+import { ConfigLoader } from "./ConfigLoader";
+import { AutomationConnexion } from "./AutomationConnexion";
 
-const NAME: string = process.env.EVNTBOARD_NAME || "automation";
-const HOST: string = process.env.EVNTBOARD_HOST || "localhost";
-const PORT: number = process.env.EVNTBOARD_PORT
-  ? parseInt(process.env.EVNTBOARD_PORT)
-  : 5001;
+const main = async () => {
+  const configLoader = new ConfigLoader();
+  await configLoader.load();
 
-const evntCom = new EvntComNode({
-  name: NAME,
-  port: PORT,
-  host: HOST,
-});
+  const conf = configLoader.getConfig();
 
-// keyboard
+  new AutomationConnexion(conf.host, conf.port, conf.name || "automation");
+};
 
-evntCom.expose("setKeyboardDelay", async (ms: number) =>
-  robot.setKeyboardDelay(ms)
-);
-
-evntCom.expose("keyTap", async (key: string, modifier?: string | string[]) =>
-  robot.keyTap(key, modifier)
-);
-
-evntCom.expose(
-  "keyToggle",
-  async (key: string, down: string, modifier?: string[]) =>
-    robot.keyToggle(key, down, modifier)
-);
-
-evntCom.expose("typeString", async (message: string) =>
-  robot.typeString(message)
-);
-
-evntCom.expose("typeStringDelayed", async (message: string, cpm: number) =>
-  robot.typeStringDelayed(message, cpm)
-);
-
-// mouse
-
-evntCom.expose("setMouseDelay", async (ms: number) => robot.setMouseDelay(ms));
-
-evntCom.expose("moveMouse", async (x: number, y: number) =>
-  robot.moveMouse(x, y)
-);
-
-evntCom.expose("moveMouseSmooth", async (x: number, y: number) =>
-  robot.moveMouseSmooth(x, y)
-);
-
-evntCom.expose("mouseClick", async (side: string, double: boolean) =>
-  robot.mouseClick(side, double)
-);
-
-evntCom.expose("mouseToggle", async (down: string, button: string) =>
-  robot.mouseToggle(down, button)
-);
-
-evntCom.expose("dragMouse", async (x: number, y: number) =>
-  robot.dragMouse(x, y)
-);
-
-evntCom.expose("getMousePos", async () => robot.getMousePos());
-
-evntCom.expose("scrollMouse", async (x: number, y: number) =>
-  robot.scrollMouse(x, y)
-);
-
-// screen
-
-evntCom.expose("getPixelColor", async (x: number, y: number) =>
-  robot.getPixelColor(x, y)
-);
-
-evntCom.expose("getScreenSize", async () => robot.getScreenSize());
-
-evntCom.expose(
-  "screenCapture",
-  async (x: number, y: number, width: number, height: number) =>
-    robot.screen.capture(x, y, width, height)
-);
+main();
